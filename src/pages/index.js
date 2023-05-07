3fcb70c58d7099954eac46600c98e43570f1ede1
Dasha Tarasova <tdariaa@mail.ru>
2023-05-07 16:47:21 +0300

    webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import { initialCards } from './constants.js';
-import Card from './Card.js';
-import FormValidator from './FormValidator.js';
-import Section from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
-
+import './index.css';
+import { initialCards } from '../components/constants.js';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
 
 const popupSelectorProfile = '.popup_profile';
 const popupSelectorCard = '.popup_card';
@@ -33,18 +33,34 @@ const profilSelector = {
   aboutSelector: '.profile__subtitle'
 };
 
-const popupWithFormProfile = new PopupWithForm(popupSelectorProfile, submitFormPopup);
+const popupWithFormProfile = new PopupWithForm(popupSelectorProfile, submitFormPopupProfile);
 popupWithFormProfile.setEventListeners();
 
-const popupWithFormCard = new PopupWithForm(popupSelectorCard, submitFormPopup);
+const popupWithFormCard = new PopupWithForm(popupSelectorCard, submitFormPopupCard);
 popupWithFormCard.setEventListeners();
 
 const profileUserInfo = new UserInfo(profilSelector);
 
-function submitFormPopup(evt) {
+function submitFormPopupProfile(evt) {
   evt.preventDefault();
   profileUserInfo.setUserInfo(popupWithFormProfile._getInputValues());
   popupWithFormProfile.close();
+};
+
+//  карточки добавляемые пользователем
+function submitFormPopupCard(evt) {
+  evt.preventDefault();
+  const newCardInitial = [{ link: popupFormLink.value, name: popupFormNameCard.value }];
+  const renderNewCardList = new Section({
+    items: newCardInitial,
+    renderer: (item) => {
+      const card = new Card(item, cardTemplateID, handleCardClick);
+      const cardElement = card.generateCard();
+      renderNewCardList.addItem(cardElement);
+    }
+  }, cardTemplateSelector);
+  renderNewCardList.renderItems();
+  popupWithFormCard.close();
 };
 
 popupEditButton.addEventListener('click', function (e) {
@@ -76,22 +92,6 @@ const renderCardList = new Section({
   }
 }, cardTemplateSelector);
 renderCardList.renderItems();
-
-//  карточки добавляемые пользователем
-popupFormCard.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const newCardInitial = [{ link: popupFormLink.value, name: popupFormNameCard.value }];
-  const renderNewCardList = new Section({
-    items: newCardInitial,
-    renderer: (item) => {
-      const card = new Card(item, cardTemplateID, handleCardClick);
-      const cardElement = card.generateCard();
-      renderNewCardList.addItem(cardElement);
-    }
-  }, cardTemplateSelector);
-  renderNewCardList.renderItems();
-  popupWithFormCard.close();
-});
 
 // валидация
 const formValidatorProfil = new FormValidator(validationConfig, popupFormProfile);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/pages/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My App',
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+          'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}
