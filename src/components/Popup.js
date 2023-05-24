1d7a366791266d1526272412349cd1d50b33d233
Dasha Tarasova <tdariaa@mail.ru>
2023-05-24 09:02:33 +0300

    fix: button

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,6 +1,7 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._submitButton = this._popup.querySelector('.popup__button');
     this._handleEscClose = this._handleEscClose.bind(this);
   }
 
